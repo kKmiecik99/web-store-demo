424891b05a037fe6f81e1d2019167bb16c00aff8
KF <k.falinski@sdacademy.pl>
2020-03-07 11:42:19 +0100

    first init

---
+++ src/main/java/pl/sda/intermediate25/Customer.java
@@ -0,0 +1,9 @@
+package pl.sda.intermediate25;
+
+public class Customer {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+}
