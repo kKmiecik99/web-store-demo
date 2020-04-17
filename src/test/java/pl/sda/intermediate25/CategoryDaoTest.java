962decb091d33bb9567f8f0afb3f9ad4dc548b97
KF <k.falinski@sdacademy.pl>
2020-04-17 22:08:08 +0200

    finished

--- src/test/java/pl/sda/intermediate25/CategoryDaoTest.java
+++ src/test/java/pl/sda/intermediate25/CategoryDaoTest.java
@@ -8,12 +8,30 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CategoryDaoTest {
+
+    CategoryDao categoryDao = CategoryDao.getInstance();
+
+
     @Test
     void shouldProperlyPopulateParentIds() {
         int expectedParentId = 4;
         int childId = 6;
 
-        CategoryDao categoryDao = CategoryDao.getInstance();
+        List<Category> categories = categoryDao.getCategories();
+        Category category = categories.stream()
+                .filter(c -> c.getId().equals(childId))
+                .findFirst()
+                .orElseThrow(() -> new AssertionError());
+
+        Assertions.assertEquals(expectedParentId, category.getParentId());
+    }
+
+    @Test
+    void shouldProperlyPopulateParentIds2() {
+        int expectedParentId = 4;
+        int childId = 6;
+
+//        CategoryDao categoryDao = CategoryDao.getInstance();
         List<Category> categories = categoryDao.getCategories();
         Category category = categories.stream()
                 .filter(c -> c.getId().equals(childId))
